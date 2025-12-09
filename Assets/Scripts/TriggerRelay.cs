72c2744e6102e5451d8406fdba11a08b288fb704
Ajay T Jose <ajaythoomkuzhy@gmail.com>
2025-12-09 17:19:52 +0100

    Fixed camera as well, need to fix coins not appearing glitch

--- Assets/Scripts/TriggerRelay.cs
+++ Assets/Scripts/TriggerRelay.cs
@@ -2,10 +2,19 @@ using UnityEngine;
 
 public class TriggerRelay : MonoBehaviour
 {
-    public LocomotionTechnique locomotion; // drag the OVRCameraRig (with LocomotionTechnique) here
+    public LocomotionTechnique locomotion; // drag OVRCameraRig here in Inspector
 
+    // Called when this object (AvatarRoot) hits a trigger collider
     void OnTriggerEnter(Collider other)
     {
-        if (locomotion) locomotion.AvatarTriggerEnter(other);
+        if (locomotion == null) return;
+        locomotion.AvatarTriggerEnter(other);
+    }
+
+    // Called when this object (AvatarRoot) hits a normal collider
+    void OnCollisionEnter(Collision collision)
+    {
+        if (locomotion == null) return;
+        locomotion.AvatarTriggerEnter(collision.collider);
     }
 }
